8e344a44679ecc7cd2106fd453d8402bcab6383f
josh55914 <143130293+josh55914@users.noreply.github.com>
2024-04-11 13:22:22 -0700

    removed using System.Collections; using System.Collections.Generic;   and void Start() {
    }

--- Prototype3/Assets/Scripts/SpawnManager.cs
+++ Prototype3/Assets/Scripts/SpawnManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
@@ -19,13 +17,6 @@ public class SpawnManager : MonoBehaviour
     {
         InvokeRepeating("SpawnObstacle", spawnDelay, spawnRate);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void SpawnObstacle()
     {
         Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
